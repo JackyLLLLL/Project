4d165ee2a42a52b882b0ada9163a915aac4d97a8
JackyLLLLL <lkktwn8331@yahoo.com.tw>
2022-04-01 16:35:15 +0800

    220401update

--- LINE Notify/LINE Notify.py
+++ LINE Notify/LINE Notify.py
@@ -2,7 +2,9 @@ import requests
 import json
 import time
 from datetime import datetime
-def get_data():
+from time import sleep
+
+def get_data(i):
     global result
 
     url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-077"
@@ -22,10 +24,12 @@ def get_data():
   
        
         location = data["records"]["locations"][0]["location"][0]["locationName"] #地區
-        weather_state = data["records"]["locations"][0]["location"][0]["weatherElement"][6]["time"]
+        weather_status = data["records"]["locations"][0]["location"][0]["weatherElement"][6]["time"]
 
-        weather_state = weather_state[2]
-    
+        
+        
+        weather_state = weather_status[i]
+          
         dateTime = weather_state["startTime"][0:11]
         startTime = weather_state["startTime"][11:-3]
         endTime = weather_state["endTime"][11:16]
@@ -38,19 +42,18 @@ def get_data():
         comfort =value[3]
         wind = value[4]
         humidy = value[5]
-
-        
-        
+       
         result = (f"\n日期:{dateTime}\n時間:{startTime}~{endTime}\n狀態:{weather_state}\n降雨機率:{rain_prob}\n目前溫度:{temp}°C\n體感:{comfort}\n風向:{wind}\n{humidy}")
         print(result)
-     
+
+                 
         
 
-        t = time.localtime()   
+        #t = time.localtime()   
         #ymd   = time.strftime("%Y.%m.%d",t)
         #date  = time.strftime("%Y%m%d",t)
         #hour  = time.strftime("%H",t)
-        current_time = str(time.strftime("%H:%M",t))
+        #current_time = str(time.strftime("%H:%M",t))
         #sec_time = datetime.now().strftime('%H:%M:%S:%f')[:-4]
 
         #return result
@@ -71,8 +74,8 @@ def line_notify():
 
     api = "CWB-3E23CE1A-37CB-4F82-8931-D3D85797A941"
     token ="QJUoBshGivcJs2cfhcopkuXhdJf6F177tLHF8hdeqFy"
-    message = "Hello! 測試一下串接LINE的API!"
-    message = result
+    message = "Hello! Elsa"
+    #message = result
    
 
     # line notify所需資料
@@ -88,7 +91,32 @@ def line_notify():
     requests.post(url=line_url, headers=line_header, data=line_data)
 
 
+
+set_time="16:03"
+prevent = True
+
 if __name__ == '__main__':
 
-    get_data()
+    while True:
+        
+        t = time.localtime()
+        current_time = str(time.strftime("%H:%M",t))
+        print(f"目前時間:{current_time}")
+
+        i=0
+
+        for i in range(24):
+        
+            if current_time >= set_time and prevent == False:
+                
+                get_data(i)
+
+            prevent = False
+
+            print(i)
+            
+        break
+            #sleep(5)
+               
+        
     #line_notify()
